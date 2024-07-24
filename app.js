6b76394baf148d9fc0860dff783d6342168fedb0
patipanbank <patipanbank1234@gmail.com>
2024-07-24 21:27:15 +0700

    aa

--- app.js
+++ app.js
@@ -1,37 +1,3 @@
-// const express = require("express");
-// const path = require("path");
-// const bcrypt = require("bcrypt");
-// const app = express();
-// const { raw } = require("mysql2");
-// const login = require('./backend/routes/login');
-// const student = require('./backend/routes/student');
-// const teacher = require('./backend/routes/teacher');
-// const admin = require('./backend/routes/admin');
-// const staff = require('./backend/routes/staff');
-// const cors = require('cors');
-
-// const corsOptions = {
-//   origin: 'http://localhost:3001', // Replace with your frontend's origin
-//   credentials: true, // Allow cookies to be sent
-// };
-// app.use(cors(corsOptions));
-
-// app.use("/public", express.static(path.join(__dirname, "public")));
-// app.use(express.json());
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use("/", login);
-// app.use("/student", student);
-// app.use("/teacher", teacher);
-// app.use("/admin", admin);
-// app.use("/staff", staff);
-
-
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, function () {
-//   console.log("Server is runnint at port " + PORT);
-// });
 
 const express = require("express");
 const path = require("path");
