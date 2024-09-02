c0eb9afed7f7eb4ae91f0f0b691db7512813a842
patipanbank <152005131+patipanbank@users.noreply.github.com>
2024-09-02 23:07:19 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 const { Sequelize } = require('sequelize');
-const connection = new Sequelize("project_web","root","root",{
+const connection = new Sequelize("project_web","root","",{
     host: 'localhost',
     dialect: 'mysql'
 });
